37ae6f2b5b6e4d04069a1f633cd2d742bbe8f016
PrattayGope <prattaygope123@gmail.com>
2025-07-13 09:14:22 +0530

    half code

---
+++ Rock_paper_Scissors.py
@@ -0,0 +1,18 @@
+import random
+
+# Print multiline instruction
+print('Winning rules of the game ROCK PAPER SCISSORS are:\n'
+      + "Rock vs Paper -> Paper wins \n"
+      + "Rock vs Scissors -> Rock wins \n"
+      + "Paper vs Scissors -> Scissors wins \n")
+
+while True:
+
+    print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
+
+    # Take the input from user
+    choice = int(input("Enter your choice: "))
+
+    # Looping until user enters valid input
+    while choice > 3 or choice < 1:
+        choice = int(input('Enter a valid choice please : '))
